df44774682a1193271e14308bbc93b00d117c4d1
mfoglino <mfoglino@gmail.com>
2020-09-07 13:39:26 -0300

    last code

--- book_shop/tienda_de_libros.py
+++ book_shop/tienda_de_libros.py
@@ -1,25 +1,62 @@
+
+
+
 class Carrito(object):
 
-    _carrito = []
-    _catalogo = ["un libro", "otro libro"]
 
-
-    def __init__(self):
-        pass
+    def __init__(self, catalogo):
+        self._carrito = []
+        self._catalogo = catalogo
 
     def es_vacio(self):
         return len(self._carrito) == 0
 
-    def add_libro(self, libro):
-        if libro in self._catalogo:
-            self._carrito.append(libro)
+    def add(self, libro, cantidad=1):
+        if self.esta_en_catalogo(libro):
+            for _ in range(cantidad):
+                self._carrito.append(libro)
         else:
             raise Exception("Ese libro no pertenece a la editorial")
 
     def contiene(self, libro):
         return libro in self._carrito
 
+    def total(self):
+        sum = 0
+        for libro in self._carrito:
+            sum += self._precio_libro(libro)
+        return sum
+
+    def _precio_libro(self, libro):
+        for i in self._catalogo:
+            if i[0] == libro:
+                return i[1]
+
+
+
+
+    def esta_en_catalogo(self, libro):
+        for l in self._catalogo:
+            if l[0] == libro:
+                return True
+
+        return False
+
+
+
 
 class Libro(object):
     def __init__(self, title):
         self.title = title
+
+
+
+class Cajero(object):
+    def __init__(self, carrito):
+        self._carrito = carrito
+
+    def checkout(self):
+        if not self._carrito.es_vacio():
+            return f"Ticket:{self._carrito.total()}"
+        else:
+            raise Exception("No se puede checkout de carrito vacio")
--- test/book_shop/test_purchase.py
+++ test/book_shop/test_purchase.py
@@ -1,29 +1,37 @@
 import pytest
 
-from book_shop.tienda_de_libros import Carrito, Libro
+from book_shop.tienda_de_libros import Carrito, Libro, Cajero
+
+
+def catalogo():
+    return [("un libro", 5), ("otro libro", 10)]
+
 
 
 def test_carrito_vacio():
-    carrito = Carrito()
+    carrito = Carrito(catalogo())
     assert carrito.es_vacio()
 
 
 def test_carrito_con_un_item():
-    carrito = Carrito()
+    carrito = Carrito(catalogo())
 
     libro = "un libro"
-    carrito.add_libro(libro)
+    carrito.add(libro)
 
     assert not carrito.es_vacio()
     assert carrito.contiene(libro)
 
 def test_carrito_dos_del_mismo():
-    carrito = Carrito()
+    carrito = Carrito(catalogo())
 
     libro = "un libro"
     uno_mas = "un libro"
-    carrito.add_libro(libro)
-    carrito.add_libro(uno_mas)
+    #carrito.add_libro(libro)
+    #carrito.add_libro(uno_mas)
+
+    cantidad = 2
+    carrito.add(libro, cantidad)
 
     assert not carrito.es_vacio()
     assert carrito.contiene(libro)
@@ -31,12 +39,12 @@ def test_carrito_dos_del_mismo():
 
 
 def test_carrito_con_mas_de_un_item():
-    carrito = Carrito()
+    carrito = Carrito(catalogo())
 
     libro = "un libro"
     otro_libro = "otro libro"
-    carrito.add_libro(libro)
-    carrito.add_libro(otro_libro)
+    carrito.add(libro)
+    carrito.add(otro_libro)
 
     assert not carrito.es_vacio()
     assert carrito.contiene(libro)
@@ -46,12 +54,52 @@ def test_carrito_con_mas_de_un_item():
 
 
 def test_editorial():
-    carrito = Carrito()
+    carrito = Carrito(catalogo())
 
     no_editorial_libro = "un libro de no la editorial"
 
     with pytest.raises(Exception, match="Ese libro no pertenece a la editorial"):
-        carrito.add_libro(no_editorial_libro)
+        carrito.add(no_editorial_libro)
+
+
+
+
+def test_el_carrito_tiene_total_correcto():
+    carrito = Carrito(catalogo())
+
+    libro = "un libro"
+
+    carrito.add(libro)
+
+    assert carrito.total() == 5
+
+
+
+
+def test_checkout_carrito_ok():
+    carrito = Carrito(catalogo())
+
+    libro = "un libro"
+
+    carrito.add(libro)
+
+    cajero = Cajero(carrito)
+
+    ticket_con_total = "Ticket:5"
+    assert cajero.checkout() == ticket_con_total
+
+
+
+def test_no_se_puede_checkout_carrito_vacio():
+    carrito = Carrito(catalogo())
+
+    cajero = Cajero(carrito)
+
+    with pytest.raises(Exception, match="No se puede checkout de carrito vacio"):
+        cajero.checkout()
+
+
+
 
 
 
