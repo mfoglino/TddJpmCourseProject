36f6b9595cbf1709185f4bdc0a45fbcc6c698835
mfoglino <mfoglino@gmail.com>
2020-09-09 20:30:11 -0300

    add test password invalida

--- book_shop/mi_applicacion.py
+++ book_shop/mi_applicacion.py
@@ -1,4 +1,5 @@
 CLIENTE_INVALIDO = "Cliente invalido"
+PASSWORD_INVALIDA = "PASSWORD INVALIDA"
 
 
 class Aplicacion(object):
@@ -10,9 +11,12 @@ class Aplicacion(object):
             raise Exception(CLIENTE_INVALIDO)
         return True
 
-    def crear_carrito(self, id_cliente):
+    def crear_carrito(self, id_cliente, password):
         if not self.es_cliente_valido(id_cliente):
             raise Exception(CLIENTE_INVALIDO)
+
+        if self._usuarios[id_cliente] != password :
+            raise Exception(PASSWORD_INVALIDA)
 
     def es_cliente_valido(self, id):
         return id in self._usuarios
--- test/book_shop/test_compra_web.py
+++ test/book_shop/test_compra_web.py
@@ -1,6 +1,6 @@
 import pytest
 
-from book_shop.mi_applicacion import Aplicacion, CLIENTE_INVALIDO
+from book_shop.mi_applicacion import Aplicacion, CLIENTE_INVALIDO, PASSWORD_INVALIDA
 
 
 def get_usuarios():
@@ -14,7 +14,7 @@ def test_cuando_creo_carrito_esta_vacio():
     app = Aplicacion(get_usuarios())
 
     id_cliente = "pepe"
-    id_carrito = app.crear_carrito(id_cliente)
+    id_carrito = app.crear_carrito(id_cliente, "123")
     assert app.es_carrito_vacio(id_carrito)
 
 
@@ -24,4 +24,12 @@ def test_no_puedo_crear_un_carrito_con_un_cliente_invalido():
 
     with pytest.raises(Exception, match=CLIENTE_INVALIDO):
         id_cliente_invalido = "id666"
-        id_carrito = app.crear_carrito(id_cliente_invalido)
+        id_carrito = app.crear_carrito(id_cliente_invalido, "no importa")
+
+
+def test_un_cliente_con_password_no_valida_no_puedo_crear_un_carrito():
+    app = Aplicacion(get_usuarios())
+
+    with pytest.raises(Exception, match=PASSWORD_INVALIDA):
+        id_cliente_invalido = "pepe"
+        id_carrito = app.crear_carrito(id_cliente_invalido, "password incorrecta")
