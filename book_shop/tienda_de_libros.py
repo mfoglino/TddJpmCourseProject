34e6e44efc8fe86c8bde809e1b0c7fcd8cd713f5
mfoglino <mfoglino@gmail.com>
2020-09-09 12:07:50 -0300

    todo bastante ok

--- book_shop/tienda_de_libros.py
+++ book_shop/tienda_de_libros.py
@@ -6,6 +6,16 @@ ERROR_NO_PERTENECE_A_LA_EDITORIAL = "Ese libro no pertenece a la editorial"
 
 ERROR_TARJETA_VENCIDA = "Error, tarjeta vencida!"
 
+ERROR_TARJETA_SIN_CREDITO = "TARJETA SIN CREDITO"
+
+ERROR_MERCHANT_CAIDO = "ERROR: MERCHANT CAIDO"
+
+MERCHANT_SIN_CONEXION = "Error: Merchant sin conexion"
+
+MERCHANT_SIN_CREDITO = "Error: Tarjeta sin credito"
+
+ERROR_CARRITO_EXPIRADO = "ERRRO: CARRITO EXPIRADO"
+
 
 class Tarjeta(object):
 
@@ -60,26 +70,36 @@ class Carrito(object):
 
 
 
-class Libro(object):
+class Libro:
     def __init__(self, title):
         self.title = title
 
 
 
-class Cajero(object):
+class Cajero:
 
-    def __init__(self, carrito, tarjeta):
+    def __init__(self, carrito, tarjeta, merchant):
         self._carrito = carrito
         self._tarjeta = tarjeta
+        self._merchant = merchant
 
 
     def checkout(self):
-        if not self._carrito.es_vacio():
-            if self._es_valida():
-                return f"Ticket:{self._carrito.total()}"
-            else:
-                raise Exception(ERROR_TARJETA_VENCIDA)
-        else:
+        self.assert_carrito_no_vacio()
+
+        self.assert_tarjeta_no_valido()
+
+
+        self._merchant.debito(self._carrito.total(), self._tarjeta)
+
+        return f"Ticket:{self._carrito.total()}"
+
+    def assert_tarjeta_no_valido(self):
+        if not self._es_valida():
+            raise Exception(ERROR_TARJETA_VENCIDA)
+
+    def assert_carrito_no_vacio(self):
+        if self._carrito.es_vacio():
             raise Exception(ERROR_CHECKOUT_DE_CARRITO_VACIO)
 
     def _es_valida(self):
@@ -89,3 +109,37 @@ class Cajero(object):
         now = dt.datetime.now()
         return self._tarjeta.fecha_vencimiento < now
 
+
+
+
+class Merchant:
+    def __init__(self):
+        pass
+
+    def debito(self, amount, tarjeta):
+        pass
+
+
+
+class MerchantOK(Merchant):
+    def __init__(self):
+        pass
+
+    def debito(self, amount, tarjeta):
+        return "OK"
+
+
+class MerchantNoCredit(Merchant):
+    def __init__(self):
+        pass
+
+    def debito(self, amount, tarjeta):
+        raise Exception(ERROR_TARJETA_SIN_CREDITO)
+
+
+class MerchantCaido(Merchant):
+    def __init__(self):
+        pass
+
+    def debito(self, amount, tarjeta):
+        raise Exception(ERROR_MERCHANT_CAIDO)
